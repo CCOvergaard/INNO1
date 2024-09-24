4e4165f875637ee33b4a4376884063238bc004b2
CCOvergaard <cach22ad@student.cbs.dk>
2024-09-24 20:49:11 +0200

    update

--- Project1/App.js
+++ Project1/App.js
@@ -2,23 +2,24 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
-import ViewOne from './screens/ViewOne';
-import ViewTwo from './screens/ViewTwo';
+import skaerm1 from './screens/skaerm1';
+import skaerm2 from './screens/skaerm2';
 
 const Stack = createStackNavigator(); // Opret en Stack Navigator
 
 export default function App() {
   return (
-    <NavigationContainer>  // Omgiver hele appen med NavigationContainer
-      <Stack.Navigator initialRouteName="Home">  // Definerer de forskellige skærme i stacken
-        <Stack.Screen name="Home" component={HomeScreen} />  // Hjemmeskærmen
-        <Stack.Screen name="ViewOne" component={ViewOne} />  // Skærm 1
-        <Stack.Screen name="ViewTwo" component={ViewTwo} />  // Skærm 2
+    // Omgiver hele appen med NavigationContainer
+    <NavigationContainer>
+      {/* Definerer de forskellige skærme i stacken */}
+      <Stack.Navigator initialRouteName="Home">
+        {/* Hjemmeskærmen */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Skærm 1 */}
+        <Stack.Screen name="skaerm1" component={skaerm1} />
+        {/* Skærm 2 */}
+        <Stack.Screen name="skaerm2" component={skaerm2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
-// alle de steder hvor kommentarerne er vide tror jeg er fordi formatering er gået galt, skal fikses for at koden kører korrekt
